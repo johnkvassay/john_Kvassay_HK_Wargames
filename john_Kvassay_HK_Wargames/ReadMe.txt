4b322890d1fdbc9295c461c7ad6f2e45875bee73
JOHN KVASSAY <JOHKVASS@uat.edu>
2018-02-09 11:38:30 -0700

    Add project files.

---
+++ john_Kvassay_HK_Wargames/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : john_Kvassay_HK_Wargames Project Overview
+========================================================================
+
+AppWizard has created this john_Kvassay_HK_Wargames application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your john_Kvassay_HK_Wargames application.
+
+
+john_Kvassay_HK_Wargames.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+john_Kvassay_HK_Wargames.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+john_Kvassay_HK_Wargames.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named john_Kvassay_HK_Wargames.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ john_Kvassay_HK_Wargames/john_Kvassay_HK_Wargames.cpp
@@ -0,0 +1,148 @@
+// HK_Ariel_WarGames.cpp : Defines the entry point for the console application.
+//
+// Precompiled Header
+#include "stdafx.h"
+// Imports
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	// Binary Search Variables
+	// The letter b at the end of the variable indicates that the variabvle is used in the bbinary search
+	bool isenemylocated_binary = false;
+	int pingnumber_binary = 0;
+
+	// Search Grid
+	int gridMax = 64;
+	int gridMin = 1;
+
+	// Linear Search Variables 
+	bool isenemylocated_Linear = false;
+	int pingnumber_Linear = 0;
+	int targetGuess_Linear = 0;
+
+	// Human Player Variables
+	bool isenemylocated_Human = false;
+	int pingnumber_Human = 0;
+	int targetGuess_Human;
+	
+	
+	
+	// Seed random number generator
+	srand(static_cast<unsigned int> (time(0)));
+
+	// Makes a defualt enemy Location on an 8x8 grid
+	int enemyLocation = rand() % gridMax + gridMin;
+
+	
+	// Human Player Here
+	while (isenemylocated_Human == false)
+	{
+		
+		//Prompt Human to enter a number
+		cout << "Enter A number that is between 1 and 64." << endl;
+		// Gets the number entered by the human player and sets targetGuess_Human 
+		cin >> targetGuess_Human;
+
+		pingnumber_Human++;
+
+		if (targetGuess_Human != enemyLocation)
+		{
+			cout << "Attempt " << pingnumber_Human << endl;
+			cout << "Checking Square " << targetGuess_Human << endl;
+			cout << "No Hostiles Detected" << endl;
+		}
+
+		else if (targetGuess_Human = enemyLocation)
+		{
+			cout << "Attempt " << pingnumber_Human << endl;
+			cout << "Checking Square " << targetGuess_Human << endl;
+			cout << "Target Found at " << targetGuess_Human << endl;
+			isenemylocated_Human = true;
+		}
+
+		else
+		{
+			// Breaks loop if something went wrong
+			cout << "Malfunction Detected" << endl;
+			cout << "Please Return to IT for help" << endl;
+			isenemylocated_Human = true;
+
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// Linear Search Pattern Code
+	while (isenemylocated_Linear == false)
+	{
+		targetGuess_Linear = ++targetGuess_Linear;
+		pingnumber_Linear++;
+
+		if (targetGuess_Linear != enemyLocation)
+		{
+			cout << "Attempt " << pingnumber_Linear << endl;
+			cout << "Checking Square " << targetGuess_Linear << endl;
+			cout << "No Hostiles Detected" << endl;
+		}
+
+		else if (targetGuess_Linear = enemyLocation)
+		{
+			cout << "Attempt " << pingnumber_Linear << endl;
+			cout << "Checking Square " << targetGuess_Linear << endl;
+			cout << "Target Found at " << targetGuess_Linear << endl;
+			isenemylocated_Linear = true;
+		}
+
+		else 
+		{
+			// Breaks loop if something went wrong
+			cout << "Malfunction Detected" << endl;
+			cout << "Please Return to IT for help" << endl;
+			isenemylocated_Linear = true;
+
+		}
+	}
+
+	// Final Results
+	cout
+
+
+	
+	
+	system("pause");
+
+	return 0;
+}
+
